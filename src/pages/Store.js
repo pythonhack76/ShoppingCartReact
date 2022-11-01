fc9667ddf76f0535ab724cd090c781b9277a6305
Python The Hack <lrulvoni@gmail.com>
2022-11-01 17:36:24 +0100

    update modal

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,19 +1,20 @@
 import { Row, Col} from 'react-bootstrap';
 import { productsArray } from '../productStore';
+import ProductCard from '../components/ProductCard';
 
 function Store() {
 
     return (
         <> 
         
-        <h1>Benvenuto in Store!</h1>
+        <h1 align="center" class="p-3">Benvenuto in Store!</h1>
         <Row xs={1} md={3} className="g-4">
 
 
             {productsArray.map((product, idx) => (
 
-<Col align="center">
-<h1>{product.title}</h1>
+<Col align="center" key={idx}>
+<ProductCard product={product} />
 </Col>
             ))}
 
